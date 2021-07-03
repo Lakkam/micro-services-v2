2be4842dee04716ec3a35faf6a647512e1965cfc
LakkamDev <rangaswamy.lakkam@gmail.com>
2021-07-03 17:23:26 +0530

    Spring API Gateway

---
+++ api-gateway/src/test/java/com/lakkam/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakkam.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
